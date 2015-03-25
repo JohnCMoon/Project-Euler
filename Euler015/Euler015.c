a380a4fb2d1c31e0d3fd8d8c8d3c5fb4635ede89
John Moon <johnmoon@live.com>
2015-03-25 00:28:10 -0700

    Starting to think about Euler15.

---
+++ Euler015/Euler015.c
@@ -0,0 +1,23 @@
+/*
+ *   
+ *   Problem 15
+ *   
+ *      Published on Friday, 19th April 2002, 06:00 pm; Solved by 103045
+ *   
+ *      Starting in the top left corner of a 2×2 grid, and only being able to
+ *      move to the right and down, there are exactly 6 routes to the bottom
+ *      right corner.
+ *   
+ *      How many such routes are there through a 20×20 grid?
+ *      
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main()
+{
+
+	return 0;
+}
